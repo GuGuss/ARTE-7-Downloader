db2bf99d4e46f8491179e0e7a2ad81a426949f7b
Augustin Delaporte <GuGuss@users.noreply.github.com>
2017-08-10 16:30:20 +0200

    Switch to HTTPS domain

--- arte-downloader.user.js
+++ arte-downloader.user.js
@@ -2,8 +2,8 @@
 // @name        Arte+7 Downloader
 // @namespace   GuGuss
 // @description Download videos or get stream link of ARTE programs in the selected language.
-// @include     http://*.arte.tv/*
-// @version     2.6
+// @include     https://*.arte.tv/*
+// @version     2.7
 // @updateURL   https://github.com/GuGuss/ARTE-7-Downloader/raw/master/arte-downloader.user.js
 // @grant       GM_xmlhttpRequest
 // @icon        http://www.arte.tv/favicon.ico
